acea2860162691c23ff3064d4cd222fbb8451e84
Maksim Akimychev <101060239+akimychau@users.noreply.github.com>
2022-06-26 20:06:51 +0100

    Урок 2 начали | добавили ViewBinding в активити и во фрагменте

---
+++ app/src/main/java/ru/akimychev/weatherapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.akimychev.weatherapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.akimychev.weatherapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
