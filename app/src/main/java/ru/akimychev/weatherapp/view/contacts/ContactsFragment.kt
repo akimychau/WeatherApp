9b7809ee2b0ebb3abd3c49c4f49b107042598e2f
Maksim Akimychev <101060239+akimychau@users.noreply.github.com>
2022-08-06 15:19:13 +0200

    Урок 9 | научились запрашивать разрешение на доступ к контактам у пользователя

--- app/src/main/java/ru/akimychev/weatherapp/view/contacts/ContactsFragment.kt
+++ app/src/main/java/ru/akimychev/weatherapp/view/contacts/ContactsFragment.kt
@@ -1,24 +1,19 @@
 package ru.akimychev.weatherapp.view.contacts
 
+import android.Manifest
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import coil.ImageLoader
-import coil.decode.SvgDecoder
-import coil.request.ImageRequest
-import com.squareup.picasso.Picasso
-import ru.akimychev.weatherapp.R
 import ru.akimychev.weatherapp.databinding.FragmentContactsBinding
-import ru.akimychev.weatherapp.databinding.FragmentDetailsBinding
-import ru.akimychev.weatherapp.domain.Weather
-import ru.akimychev.weatherapp.viewmodel.details.DetailsFragmentAppState
-import ru.akimychev.weatherapp.viewmodel.details.DetailsViewModel
 
 class ContactsFragment : Fragment() {
 
@@ -37,7 +32,49 @@ class ContactsFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
 
+    private fun checkPermission() {
+        val permResult =
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)
+        if (permResult == PackageManager.PERMISSION_GRANTED) {
+            getContacts()
+        } else {
+            permissionRequest(Manifest.permission.READ_CONTACTS)
+        }
+    }
+
+    private val REQUEST_CODE_READ_CONTACTS = 999
+
+    private fun permissionRequest(permission: String) {
+        requestPermissions(arrayOf(permission), REQUEST_CODE_READ_CONTACTS)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_READ_CONTACTS) {
+            for (pIndex in permissions.indices) {
+                if (permissions[pIndex] == Manifest.permission.READ_CONTACTS && grantResults[pIndex] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun getContacts() {
+        val contentResolver: ContentResolver = requireContext().contentResolver
+        val cursorWithContacts: Cursor? = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME + "ASC"
+        )
     }
 
     override fun onDestroyView() {
